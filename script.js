442783c47bf9fb07377e62dc6622beb11879b517
riskecrp <riskecrp@gmail.com>
2026-01-23 03:22:17 -0600

    Refactor generateBBCode function for clarity

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 function generateBBCode() {
-    // Get values (Mapping just a few as examples, add the rest similarly)
-    const fName = document.getElementById('fName').value;
-    const lName = document.getElementById('lName').value;
     const gender = document.getElementById('gender').value;
-    const motivation = document.getElementById('motivation').value;
-    
-    // Checkbox logic for Gender
-    const maleX = gender === "Male" ? "X" : " ";
-    const femaleX = gender === "Female" ? "X" : " ";
-    const otherX = gender === "Other" ? "X" : " ";
+    const ai = document.getElementById('aiConfirm').value;
 
     const bbCode = `[img]https://i.imgur.com/a32OdzR.png[/img]
 [lsemssubtitle]SECTION 1 - PERSONAL INFORMATION[/lsemssubtitle]
@@ -16,15 +8,15 @@ function generateBBCode() {
 [b]1.1) Name[/b]
 [list=none]
 Title: ${document.getElementById('title').value}
-First Name: ${fName}
-Last Name: ${lName}
+First Name: ${document.getElementById('fName').value}
+Last Name: ${document.getElementById('lName').value}
 [/list]
 
-[b]1.2) Gender - (use 'X' to select):[/b]
+[b]1.2) Gender:[/b]
 [list=none]
-[${femaleX}] Female
-[${maleX}] Male
-[${otherX}] Other
+[${gender === 'Female' ? 'X' : ' '}] Female
+[${gender === 'Male' ? 'X' : ' '}] Male
+[${gender === 'Other' ? 'X' : ' '}] Other
 [/list]
 
 [b]1.3) Date & Place of Birth [/b]
@@ -33,12 +25,12 @@ Place: ${document.getElementById('pob').value}
 Date: ${document.getElementById('dob').value}
 [/list]
 
-[b]1.5) Contact details[/b]
+[b]1.6) Convictions:[/b]
 [list=none]
-Phone Number: ${document.getElementById('phone').value}
+${document.getElementById('crimRecord').value}
 [/list]
 
-[b]1.7) Please provide proof of your identification and license.[/b]
+[b]1.7) ID & License:[/b]
 [list=none]
 [URL=${document.getElementById('idLink').value}]Attached Documents[/URL]
 [/list]
@@ -46,25 +38,30 @@ Phone Number: ${document.getElementById('phone').value}
 
 [lsemssubtitle]SECTION 3 - GENERAL QUESTIONS[/lsemssubtitle]
 [divbox=white]
-[b]3.1 Motivation behind joining?[/b]
+[b]3.1 Motivation:[/b]
 [list=none]
-${motivation}
+${document.getElementById('motivation').value}
 [/list]
 [/divbox]
 
 [lsemssubtitle](( SECTION 4 - OOC INFORMATION ))[/lsemssubtitle]
 [divbox=white]
-[b]4.2) ECRP Forum Profile[/b]
+[b]4.1) Player Information[/b]
 [list=none]
-Forum Profile Link: [url=${document.getElementById('forumLink').value}]Here[/url]
+Discord Username: ${document.getElementById('discord').value}
+[/list]
+
+[b]4.10) AI Confirmation:[/b]
+[list=none]
+[${ai === 'Yes' ? 'X' : ' '}] Yes, I do.
+[${ai === 'AI' ? 'X' : ' '}] I utilized AI-based text generators.
+[${ai === 'Grammar' ? 'X' : ' '}] I utilized Grammar-assist software.
 [/list]
 [/divbox]
 [LSEMSfooter][/LSEMSfooter]`;
 
-    // Copy to clipboard
     navigator.clipboard.writeText(bbCode).then(() => {
-        alert("Application BBCode Copied!");
-        // Redirect to New Topic in the correct sub-forum
+        alert("BBCode Copied!");
         window.open("https://gov.eclipse-rp.net/posting.php?mode=post&f=575", "_blank");
     });
 }
